580f5b8ec5b3e5530c5dcabc1e5f0d8278e1ad4f
Sebouh Paul <sebouh.paul@gmail.com>
2023-06-23 11:40:25 -0700

    commented out DETECTOR_PATH stuff

--- material_budget.sh
+++ material_budget.sh
@@ -1,11 +1,11 @@
 
-phimin=3.14159
+phimin=-3.14159
 phimax=3.14159
 nphi=1
 
 
-for xmlfile in beampipe_and_flanges_only.xml flange_-120cm_only.xml flange_-270cm_only.xml; do
-    for eta in -3.3 -3.35 -3.4 -3.45 -3.5 -3.55 -3.6 -3.65 -3.7 -3.75 -3.8 -3.85 -3.9 -3.95 -4.0 -4.05 -4.1 -4.15 -4.2 -4.25 -4.3 -4.35 -4.4 -4.45 -4.5 -4.55 -4.6; do
+for xmlfile in beampipe_and_flanges_only.xml flange_-120cm_only.xml flange_-270cm_only.xml beampipe_flanges_and_ecal_endcap.xml; do
+    for eta in -3.3 -3.35 -3.4 -3.45 -3.5 -3.55 -3.6 -3.65 -3.7 -3.75 -3.8 -3.85 -3.9 -3.95 -4.0 -4.05 -4.1 -4.15 -4.2 -4.25 -4.3 -4.35 -4.4 -4.45 -4.5 -4.55 -4.6 -4.65 -4.7 -4.75 -4.8 -4.85 -4.9; do
 	python material_budget.py $xmlfile $eta $phimin $phimax $nphi &
     done
 done
